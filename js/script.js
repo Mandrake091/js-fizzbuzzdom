b99cb7f99f5766bedb5043eb89f744a1ff69a797
Paolo Cannistraro <wasdgamer91@gmail.com>
2022-03-28 18:17:06 +0200

    complete

--- js/script.js
+++ js/script.js
@@ -26,40 +26,33 @@ for (let i = 1; i <= maxNum; i++) {
     col.setAttribute('class', 'col-12 col-sm-6 col-md-4 col-lg-2');
 
     const square = document.createElement('div');
-    square.setAttribute('class', 'square color-0');
+
 
 
     if (i % 3 == 0 && i % 5 == 0) {
-
         console.log((i) + " : FizzBuzz");
-        
-        square.innerHTML += `<h1>${[i]}</h1>`
-
+        square.innerHTML += `<h1>FizzBuzz</h1>`
+        square.setAttribute('class', 'square color-0');
         for (let c = 0; c < i.length; c++) {
         }
 
-
-
     } else if (i % 5 === 0) {
         console.log((i) + " : Buzz");
-        square.innerHTML += `<h1>${[i]}</h1>`
+        square.innerHTML += `<h1>Buzz</h1>`
         square.setAttribute('class', 'square color-1');
-
         for (let c = 0; c < i.length; c++) {
         }
 
     } else if (i % 3 === 0) {
         console.log((i) + " : Fizz");
-        square.innerHTML += `<h1>${[i]}</h1>`
+        square.innerHTML += `<h1>Fizz</h1>`
         square.setAttribute('class', 'square color-2');
-
         for (let c = 0; c < i.length; c++) {
         }
 
     } else {
         square.innerHTML += `<h1>${[i]}</h1>`
         square.setAttribute('class', 'square color-3');
-        console.log((i));
     }
 
     col.append(square);
