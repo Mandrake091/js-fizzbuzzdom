56e030c9dd7002c7f82292316ab3474f3cbcbf2b
Paolo Cannistraro <wasdgamer91@gmail.com>
2022-03-28 17:58:11 +0200

    almost complete

--- js/script.js
+++ js/script.js
@@ -14,23 +14,56 @@ F & I & H & C -->Z[Fine]
 
 
 let maxNum = 100;
+const app = document.getElementById('app');
+const row = document.createElement('div');
+row.setAttribute('class', 'row g-3')
+app.append(row);
 
 for (let i = 1; i <= maxNum; i++) {
 
+    const col = document.createElement('div');
+
+    col.setAttribute('class', 'col-12 col-sm-6 col-md-4 col-lg-2');
+
+    const square = document.createElement('div');
+    square.setAttribute('class', 'square color-0');
+
+
     if (i % 3 == 0 && i % 5 == 0) {
+
         console.log((i) + " : FizzBuzz");
+        
+        square.innerHTML += `<h1>${[i]}</h1>`
+
+        for (let c = 0; c < i.length; c++) {
+        }
+
+
 
     } else if (i % 5 === 0) {
         console.log((i) + " : Buzz");
+        square.innerHTML += `<h1>${[i]}</h1>`
+        square.setAttribute('class', 'square color-1');
+
+        for (let c = 0; c < i.length; c++) {
+        }
 
     } else if (i % 3 === 0) {
         console.log((i) + " : Fizz");
+        square.innerHTML += `<h1>${[i]}</h1>`
+        square.setAttribute('class', 'square color-2');
+
+        for (let c = 0; c < i.length; c++) {
+        }
 
     } else {
+        square.innerHTML += `<h1>${[i]}</h1>`
+        square.setAttribute('class', 'square color-3');
         console.log((i));
     }
 
-
-
-
+    col.append(square);
+    row.append(col);
 }
+
+app.append(row);
