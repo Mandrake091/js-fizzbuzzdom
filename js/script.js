65db1dc17cc732a7641286b169d119443ba46231
Paolo Cannistraro <wasdgamer91@gmail.com>
2022-03-29 09:37:41 +0200

    added flowchart in readme

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-/*
-graph TD
-START --> A((Ciclare numeri da 1 a 100))
-A-->B{I numeri sono divisibili per 3 e per 5?}
-B-->|si|C[Stampa:FizzBuzz]
-B-->|no|D{Divisibile per 5?}
-D-->|no|E{Divisibile per 3?}
-D-->|si|H["Stampa:Buzz"]
-E-->|si|I[Stampa:Fizz]
-E-->|no|F[Stampa: Numeri rimanenti]
-F & I & H & C -->Z[Fine]
-*/
 
 let maxNum = 100;
 const app = document.getElementById('app');
